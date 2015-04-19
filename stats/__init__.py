73e25ab070febe9fa27d708716a118095b37a62e
Yutian Li <hotpxless@gmail.com>
2015-04-19 21:37:27 +0800

    [master] structure python code

---
+++ stats/__init__.py
@@ -0,0 +1,1 @@
+from .sliding_ratio import *
---
+++ stats/sliding_ratio.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.4
+import numpy as np
+
+def slidingRatio(data, windowSize):
+    return np.asarray([data[i] / np.min(data[i - windowSize + 1:i + 1]) for i in range(windowSize - 1, len(data))])
+
