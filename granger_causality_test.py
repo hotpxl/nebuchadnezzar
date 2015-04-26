d1ac518c68dc4dcc2810dc6ef966f58fedbd6910
Yutian Li <hotpxless@gmail.com>
2015-04-27 00:56:08 +0800

    [master] WIP

--- granger_causality_test.py
+++ granger_causality_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.4
-import json
 import numpy as np
 import statsmodels.tsa.stattools as stattools
+import matplotlib.pyplot as plt
 import stats.data
 import stats.plot
 import stats.preprocess
@@ -9,16 +9,25 @@ import stats.preprocess
 threshold = 0.05
 
 sse_indices = stats.data.sse_indices()
+bar_width=0.2
+results = []
 for index in sse_indices:
-    data = stats.data.get_merged(index, 'volume', 'readCount')
+    x = stats.data.get_merged(index, 'volume', 'readCount')
     res = stattools.grangercausalitytests(x, 7, verbose=False)
-    bestResults = []
-    for i in res:
-        good = 0
-        for j in res[i][0]:
-            if res[i][0][j][1] <= threshold:
-                good += 1
-        bestResults.append((i, good))
-    bestResults.sort(key=lambda x: -x[1])
-    print(sse, bestResults[0])
-
+    cur = [
+        res[7][0]['ssr_chi2test'][1],
+        res[7][0]['params_ftest'][1],
+        res[7][0]['lrtest'][1],
+        res[7][0]['ssr_ftest'][1],
+    ]
+    results.append(cur)
+fig, ax = plt.subplots()
+index = np.arange(len(results))
+plt.bar(index, np.asarray(results)[:, 0].flatten(), bar_width, color='b', label='ssr_chi2test')
+plt.bar(index + bar_width, np.asarray(results)[:, 1].flatten(), bar_width, color='y', label='params_ftest')
+plt.bar(index + 2 * bar_width, np.asarray(results)[:, 2].flatten(), bar_width, color='r', label='lrtest')
+plt.bar(index + 3 * bar_width, np.asarray(results)[:, 3].flatten(), bar_width, color='g', label='ssr_ftest')
+plt.xlabel('Stock')
+plt.ylabel('Test')
+plt.legend(loc=9)
+plt.show()
--- stats/plot.py
+++ stats/plot.py
@@ -1,28 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates
+from datetime import date
 
-def twin_x(data, x=None):
+def twin_x(data, labels=None, x=None):
     if not isinstance(data, np.ndarray):
         data = np.asarray(data)
     assert(data.ndim == 2)
     assert(data.shape[1] == 2)
     if x == None:
         x = np.arange(data.shape[0])
-    fix, ax1 = plt.subplots()
+    fig, ax1 = plt.subplots()
     ax1.plot(x, data[:, 0], 'b')
     for i in ax1.get_yticklabels():
         i.set_color('b')
     ax1.get_yaxis().get_offset_text().set_color('b')
-    ax1.set_ylabel('volume', color='b')
-    ax1.fmt_xdata = matplotlib.dates.DateFormatter('%Y-%m-%d')
-    fig.autofmt_xdate()
+    try:
+        ax1.set_ylabel(labels[0], color='b')
+    except:
+        ax1.set_ylabel('', color='b')
+    if isinstance(x[0], date):
+        ax1.fmt_xdata = matplotlib.dates.DateFormatter('%Y-%m-%d')
+        fig.autofmt_xdate()
     ax2 = ax1.twinx()
     ax2.plot(x, data[:, 1], 'r')
     for i in ax2.get_yticklabels():
         i.set_color('r')
     ax2.get_yaxis().get_offset_text().set_color('r')
-    ax2.set_ylabel('read count', color='b')
+    try:
+        ax2.set_ylabel(labels[1], color='r')
+    except:
+        ax2.set_ylabel('', color='r')
     plt.show()
 
 def single_x(data, x=None):
