a731b44d5f494a9067868ee1e3f12a640012f3c8
Yutian Li <hotpxless@gmail.com>
2015-05-02 21:00:50 +0800

    [master] new scraper

---
+++ scraper/guba.coffee
@@ -0,0 +1,85 @@
+#!/usr/bin/env coffee
+fs = require 'fs'
+request = require 'request'
+Q = require 'q'
+_ = require 'lodash'
+moment = require 'moment'
+winston = require 'winston'
+
+logger = new (winston.Logger)(
+  transports: [
+    new (winston.transports.Console)(
+      level: 'debug'
+      colorize: true
+      label: module.filename
+    )
+  ]
+)
+
+requestAsync = (url) ->
+  deferred = Q.defer()
+  request url, (err, response, body) ->
+    deferred.reject err if err
+    deferred.reject new Error("#{url} returned status code #{response.statusCode}") if response.statusCode != 200
+    deferred.resolve body
+  deferred.promise
+
+makeUrl = (symbol, page) ->
+  "http://m.guba.eastmoney.com/getdata/articlelist?code=#{symbol}&count=200&thispage=#{page}"
+
+stripSingleEntry = (symbol, entry) ->
+  symbol: symbol
+  id: entry.post_id
+  title: entry.post_title
+  content: entry.post_content
+  publishTime: moment(entry.post_publish_time).toISOString()
+  lastReplyTime: moment(entry.post_last_time).toISOString()
+  clickCount: entry.post_click_count
+  commentCount: entry.post_comment_count
+
+dumpEntries = (entries, redis) ->
+  _.map entries, (entry) ->
+    redis.set "#{entry.symbol}:#{entry.id}", JSON.stringify(entry)
+
+parseSinglePage = (symbol, page, redis) ->
+  url = makeUrl symbol, page
+  requestAsync url
+  .then (data) ->
+    data = JSON.parse data
+    entries = _.map data.re, (post) ->
+      stripSingleEntry symbol, post
+    dumpEntries entries, redis
+    redis.hset 'progress', symbol, page
+    logger.debug 'processed',
+      length: entries.length
+      symbol: symbol
+      page: page
+    entries.length
+
+parseSingleSymbol = (symbol, startPage, redis) ->
+  logger.debug 'starting',
+    symbol: symbol
+    startPage: startPage
+  loo = (page) ->
+    parseSinglePage symbol, page, redis
+    .then (res) ->
+      if res == 0
+        return
+      else
+        loo page + 1
+  loo startPage
+
+parseAll = (redis) ->
+  symbolList = do ->
+    JSON.parse fs.readFileSync('../data/sse_50.json', 'ascii')
+  Q.ninvoke redis, 'hgetall', 'progress'
+  .then (progress) ->
+    Q.all _map(symbolList, (i) ->
+      startPage = parseInt progress?[i] ? 1
+      parseSingleSymbol i, startPage, redis
+    )
+
+if require.main == module
+  redis = require('redis').createClient()
+  parseAll redis
+  .done()
--- vector_auto_regression.py
+++ vector_auto_regression.py
@@ -5,24 +5,43 @@ import stats.preprocess
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates
+import datetime
 from statsmodels.tsa.api import VAR, DynamicVAR
 
-d = stats.data.get_merged(600036, 'date', 'volume', 'readCount')
-# strip first few data points
-d = d[2:]
-data = pandas.DataFrame({'volume': d[:, 1].astype(float), 'readCount': d[:, 2].astype(float)})
-data.index = pandas.DatetimeIndex(d[:, 0].astype(str))
-model = VAR(data)
-lag = model.select_order()['hqic']
-length = data.values.shape[0]
-print('using lag {}'.format(lag))
-results = model.fit(lag)
-prediction = [0] * (lag - 1)
-for i in range(lag, length + 1):
-    prediction.append(results.forecast(data.values[i - lag: i], 1)[0][1])
-pred = np.asarray(prediction).reshape((length, 1))
-plt.plot(np.arange(length), pred, 'r', np.arange(length), d[:, 1], 'b')
-plt.show()
+sse_indices = stats.data.sse_indices()
+for i in sse_indices:
+    d = stats.data.get_merged(i, 'date', 'volume', 'readCount')
+    # strip first few data points
+    d = d[2:]
+    for window_size in range(3, 10):
+    # window_size = 7
+        raw_volume = d[:, 1].astype(float)
+        volume = np.concatenate((np.zeros(window_size - 1,), stats.preprocess.sliding_ratio(raw_volume, window_size).astype(float)))
+        read_count = d[:, 2].astype(float)
+        data = pandas.DataFrame({'volume': volume, 'readCount': read_count})
+        data.index = pandas.DatetimeIndex(d[:, 0].astype(str))
+        model = VAR(data)
+        lag = model.select_order()['hqic']
+        length = data.values.shape[0]
+        print('using lag {}'.format(lag))
+        results = model.fit(lag)
+        # import IPython; IPython.embed()
+        prediction = [0] * (lag)
+        for j in range(lag, length):
+            prediction.append(results.forecast(data.values[j - lag: j], 1)[0][1])
+        pred = np.asarray(prediction).reshape((length, 1))
+        fig, ax = plt.subplots()
+        dates = list(map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').date(), d[:, 0]))
+        ax.plot(dates, pred, 'r', label='forecast')
+        ax.plot(dates, volume, 'b', label='real')
+        ax.fmt_xdata = matplotlib.dates.DateFormatter('%Y-%m-%d')
+        fig.autofmt_xdate()
+        ax.set_ylabel('Volume')
+        ax.legend()
+        plt.show()
+        # plt.savefig('{}_{}.png'.format(i, window_size))
+
 # stats.plot.twin_x(np.concatenate((d[:, 1].reshape((length, 1)), pred), axis=1))
 
 # import IPython; IPython.embed()
