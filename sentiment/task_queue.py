62707d927508bd8b915ea9423eec79ec4aeb1910
Yutian Li <hotpxless@gmail.com>
2015-05-11 01:04:32 +0800

    [master] external sentiment analysis

--- sentiment/task_queue.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-import argparse
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='make and set task queue')
-    args = parser.parse_args()
-    print args
