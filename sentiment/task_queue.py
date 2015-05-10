f8f149418913bd71e922ea0c3317817852cc3f0b
Yutian Li <hotpxless@gmail.com>
2015-05-10 16:26:54 +0800

    [master] task queue

---
+++ sentiment/task_queue.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='make and set task queue')
+    args = parser.parse_args()
+    print args
